11c0a90e8a0695a39c4132fe223cd5bd5f46325d
Elvis Souza <elvismariel@gmail.com>
2026-04-29 09:00:24 -0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
